a2e0d1fa3c1d971494d81f86adcf107145e88eac
Siam24857 <siamtechofficial1597@gmail.com>
2026-04-16 01:10:46 +0600

    All is done in project

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",   
+
   images: {
+    unoptimized: true,  
     remotePatterns: [
       {
         protocol: 'https',
@@ -11,7 +14,7 @@ const nextConfig = {
       },
     ],
   },
-   
+
   reactCompiler: true,
 };
 
